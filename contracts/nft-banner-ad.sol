c71952a81dc9a7f067ca486bf18deafa83af753f
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2022-01-11 14:08:26 -0700

    Set up contract structure and sudo code

--- contracts/nft-banner-ad.sol
+++ contracts/nft-banner-ad.sol
@@ -4,13 +4,21 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
 contract NFTBannerAd is Ownable, ERC721 {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter private _tokenIdCounter;
+
+    event NewNFTMinted(address sender, uint256 tokenId);
 
     constructor() ERC721("NFTBannerAd", "BA") {
-
+        console.log("This is a WIP NFT Banner Ad contract");
     }
+
+    
 }
 
+// will mint an nft to be displayed as a banner on my website
+// will emit an event when nft is minted so can update website
+//
